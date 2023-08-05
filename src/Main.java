a6ac1eb9a0a35fcb6ffc7bcde0c63c755ee2ad2d
Yurochkin Roman <118600131+Romatilo@users.noreply.github.com>
2023-08-05 20:21:39 +0700

    Main examples added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ToyShop shop = new ToyShop();
+
+        Toy car = new Toy(1, "Car", 5, 20);
+        Toy doll = new Toy(2, "Doll", 3, 15);
+        Toy ball = new Toy(3, "Ball", 7, 28);
+        Toy yoyo = new Toy(4, "Yo-yo", 4, 15);
+        Toy bear = new Toy(5, "Bear", 2, 7);
+        Toy sword = new Toy(6, "Sword", 3, 15);
+
+        shop.addToy(car);
+        shop.addToy(doll);
+        shop.addToy(ball);
+        shop.addToy(yoyo);
+        shop.addToy(bear);
+        shop.addToy(sword);
+
+        shop.changeWinningRate(3, 22);
+        shop.changeWinningRate(1, 26);
+
+        shop.play();
+        shop.play();
+        shop.play();
+        shop.play();
     }
 }
